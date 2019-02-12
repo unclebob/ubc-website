278cc34062baa5c8575767135ebf0023b3741fa4
Robert C. Martin <unclebob@cleancoder.com>
2019-02-12 11:07:15 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ubc-website
+
+FIXME
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2019 FIXME
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject ubc-website "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler ubc-website.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
