46e0476b8e573a669126b6ad3c097d3e8295365c
Robert C. Martin <unclebob@cleancoder.com>
2019-02-26 15:23:50 -0600

    Check type of categories produced by get-categories.

--- test/ubc_website/interactors/front_page_test.clj
+++ test/ubc_website/interactors/front_page_test.clj
@@ -3,6 +3,8 @@
                                   deftest
                                   testing
                                   is]]
+            [clojure.spec.alpha :as s]
+            [ubc-website.entities.products :as products]
             [ubc-website.interactors.front-page :as fp]
             [me.raynes.fs :as fs]
             ))
@@ -22,35 +24,37 @@
   (let [product-file (str directory "/" category "/" product)]
     (spit product-file description)))
 
+(defn check-categories [directory expected]
+  (let [categories (fp/get-categories directory)
+        valid (s/explain-data ::products/categories categories)]
+    (is (nil? valid))
+    (is (= expected categories))))
+
 (deftest test-get-products
   (clear-directory)
   (testing "no Categories Directory"
-    (let [categories (fp/get-categories directory)]
-      (is (= [] categories))))
+    (check-categories directory []))
 
   (testing "one category with no products"
     (clear-directory)
     (create-category "001_Category")
-    (is (= (fp/get-categories directory)
-           [{:product-category "Category"
-             :products []}])))
+    (check-categories directory [{:product-category "Category"
+                                  :products []}]))
 
   (testing "two categories with no products"
     (clear-directory)
     (create-category "002_Category2")
     (create-category "001_Category1")
-    (is (= (fp/get-categories directory)
-           [{:product-category "Category1" :products []}
-            {:product-category "Category2" :products []}])))
+    (check-categories directory [{:product-category "Category1" :products []}
+            {:product-category "Category2" :products []}]))
 
   (testing "one category one product"
     (clear-directory)
     (create-category "001_Category")
     (create-product "001_Category" "001_Product" "Description")
-    (is (= (fp/get-categories directory)
-           [{:product-category "Category"
+    (check-categories directory [{:product-category "Category"
              :products [{:product-name "Product"
-                         :product-description "Description"}]}])))
+                         :product-description "Description"}]}]))
 
   (testing "two categories three products"
     (clear-directory)
@@ -59,15 +63,11 @@
     (create-product "001_C1" "001_P1" "D1")
     (create-product "002_C2" "002_P2" "D2")
     (create-product "002_C2" "003_P3" "D3")
-    (is (= (fp/get-categories directory)
-           [{:product-category "C1"
+    (check-categories directory [{:product-category "C1"
              :products [{:product-name "P1"
                          :product-description "D1"}]}
             {:product-category "C2"
              :products [{:product-name "P2"
                          :product-description "D2"}
                         {:product-name "P3"
-                         :product-description "D3"}]}]))
-    )
-
-  )
+                         :product-description "D3"}]}])))
