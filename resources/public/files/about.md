819a6de60e0ed007d8cfbcf55830d415ebfc37de
Robert C. Martin <unclebob@cleancoder.com>
2019-04-02 11:23:44 -0500

    Tweets on screen.  About.  Some reformatting.  Start books.

---
+++ resources/public/files/about.md
@@ -0,0 +1,33 @@
+###Robert C. Martin 
+>(Uncle Bob) 
+
+Mr. Martin has been a programmer since 1970.
+
+He is a co-founder of [cleancoders.com](http://cleancoders.com), 
+offering on-line video training for software developers. 
+
+He is the founder of Uncle Bob Consulting LLC, offering software consulting, 
+training, and skill development services to major corporations worldwide.
+He served as the Master Craftsman at [8th Light inc](http://8thlight.com), a Chicago based Software Consulting firm.
+
+Mr. Martin has published dozens of articles in various trade journals, 
+and is a regular speaker at international conferences and trade shows. 
+ 
+He is also the creator of the acclaimed educational _Clean Code_ video series at 
+cleancoders.com.
+
+Mr. Martin has authored and edited many books including:
+* Designing Object Oriented C++ Applications using the Booch Method
+* Patterns Languages of Program Design 3
+* More C++ Gems
+* Extreme Programming in Practice
+* Agile Software Development: Principles, Patterns, and Practices.
+* Agile Principles, Patterns, and Practices in C#
+* UML for Java Programmers
+* Clean Code
+* The Clean Coder
+* Clean Architecture
+
+A leader in the industry of software development, Mr. Martin served 
+three years as the editor-in-chief of the C++ Report, and he served 
+as the first chairman of the Agile Alliance.
--- src/ubc_website/views/front_page.clj
+++ src/ubc_website/views/front_page.clj
@@ -4,6 +4,9 @@
     [ubc-website.presenters.front-page :as p]
     [ubc-website.presenters.util :refer [add-hiccup]]))
 
+;link from publish.twitter.com
+(def embedded-twitter-feed "<a class=\"twitter-timeline\" data-width=\"400\" data-height=\"400\" data-theme=\"light\" href=\"https://twitter.com/unclebobmartin?ref_src=twsrc%5Etfw\">Tweets by unclebobmartin</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>")
+
 (defn show [{:keys [categories events]}]
   (html5
     [:head
@@ -13,11 +16,18 @@
      [:div#header
       [:img#logo {:src "/images/cleancodelogo.png"
                   :align "left"}]
-      [:img#caricature {:src "/images/BobCaricature.jpg"
-                        :align "right"}]
+      [:a {:href "/files/about.md"}
+       [:img#caricature {:src "/images/BobCaricature.jpg"
+                        :align "right"}]]
       [:p#ubc "Uncle Bob Martin"]
-      [:p#ubc-subtitle "Software Speaker and Teacher"]]
+      [:p#ubc-subtitle "Programmer, Speaker, Teacher"]]
      (add-hiccup [:div#products] (p/present-product-categories categories))
-     (add-hiccup [:div#sidebar [:p#event-title "Up Comming Events"]]
+     [:div#sidebar
+     (add-hiccup [:p.sidebar-title "Up Comming Events"]
                  (p/present-events events))
-     ]))
+     (add-hiccup [:p.sidebar-title "Uncle Bob's Tweets"]
+                 embedded-twitter-feed)
+     (add-hiccup [:p.sidebar-title "Recommended Books"]
+                 "")
+
+     ]]))
