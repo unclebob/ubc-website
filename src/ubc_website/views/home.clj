5f1bde2de0f60cc361dd054346cc7e1c6ad29653
Robert C. Martin <unclebob@cleancoder.com>
2026-07-28 14:09:42 -0500

    Show book cover grid on front page

--- src/ubc_website/views/home.clj
+++ src/ubc_website/views/home.clj
@@ -6,24 +6,34 @@
 
 (def books
   [{:title "Clean Code, 2nd ed."
+    :isbn "9780135398579"
     :url "https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0135398576"}
    {:title "Clean Architecture"
+    :isbn "9780134494166"
     :url "https://www.amazon.com/Clean-Architecture-Craftsmans-Software-Structure/dp/0134494164"}
    {:title "Functional Design"
+    :isbn "9780138176396"
     :url "https://www.amazon.com/Functional-Design-Principles-Patterns-Practices/dp/0138176396"}
    {:title "We, Programmers"
+    :isbn "9780135344262"
     :url "https://www.amazon.com/We-Programmers-Chronicle-Coders-Robert/dp/0135344263"}
    {:title "Clean Agile"
+    :isbn "9780135781869"
     :url "https://www.amazon.com/Clean-Agile-Basics-Robert-Martin/dp/0135781868"}
    {:title "The Clean Coder"
+    :isbn "9780137081073"
     :url "https://www.amazon.com/Clean-Coder-Conduct-Professional-Programmers/dp/0137081073"}
    {:title "Agile Software Development: Principles, Patterns, and Practices"
+    :isbn "9780135974445"
     :url "https://www.amazon.com/Software-Development-Principles-Patterns-Practices/dp/0135974445"}])
 
-(defn format-book [{:keys [title url]}]
+(defn cover-url [isbn]
+  (str "https://www.informit.com/ShowCover.aspx?isbn=" isbn "&type=f"))
+
+(defn format-book [{:keys [title isbn url]}]
   [:a.book-palette-link {:href url}
-   [:span.book-palette-title title]
-   [:span.book-palette-source "Amazon"]])
+   [:img.book-cover {:src (cover-url isbn) :alt title}]
+   [:span.book-palette-title title]])
 
 (defn show []
   (page-template
