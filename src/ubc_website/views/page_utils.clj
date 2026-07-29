7d4e8e9c02ee8c108785a3e9bab4973cddd7f375
Robert C. Martin <unclebob@cleancoder.com>
2026-07-29 07:30:28 -0500

    Format bathrobe rant sidebar

--- src/ubc_website/views/page_utils.clj
+++ src/ubc_website/views/page_utils.clj
@@ -13,19 +13,19 @@
     {:src "/images/morning-bathrobe-rant.jpg"
      :alt "Morning Bathrobe Rants"}]
    [:ul.morning-bathrobe-rant-links
-    [:li [:a {:href "https://x.com/unclebobmartin/status/2081334541667410312"} "20X - 2026-07-26"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/2057064795241676838"} "tubes on pegs - 2026-05-20"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/2046578685080220074"} "Quicksilver - 2026-04-21"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/2046206145597972849"} "AI out-codes you; deal with it - 2026-04-20"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1999825947726340531"} "Truly Bizarre Failure Modes - 2025-12-13"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1982432101665484900"} "SOLID up front - 2025-10-26"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1930572242099769507"} "agile - 2025-06-05"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1929854761626436092"} "testing discipline - 2025-06-03"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1921895270322786438"} "Micro Services - 2025-05-12"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1920794352214741335"} "Write your damned tests - 2025-05-09"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1920433782563254776"} "Types - 2025-05-08"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1918276572760486249"} "OOP - 2025-05-02"]]
-    [:li [:a {:href "https://x.com/unclebobmartin/status/1917558113177108537"} "about SQL - 2025-04-30"]]]])
+    [:li [:a {:href "https://x.com/unclebobmartin/status/2081334541667410312"} "2026-07-26 20X"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/2057064795241676838"} "2026-05-20 tubes on pegs"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/2046578685080220074"} "2026-04-21 Quicksilver"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/2046206145597972849"} "2026-04-20 AI out-codes you; deal with it"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1999825947726340531"} "2025-12-13 Truly Bizarre Failure Modes"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1982432101665484900"} "2025-10-26 SOLID up front"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1930572242099769507"} "2025-06-05 agile"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1929854761626436092"} "2025-06-03 testing discipline"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1921895270322786438"} "2025-05-12 Micro Services"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1920794352214741335"} "2025-05-09 Write your damned tests"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1920433782563254776"} "2025-05-08 Types"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1918276572760486249"} "2025-05-02 OOP"]]
+    [:li [:a {:href "https://x.com/unclebobmartin/status/1917558113177108537"} "2025-04-30 about SQL"]]]])
 
 (defn page-template [content {:keys [message canonical-home]}]
   (html5
@@ -34,7 +34,7 @@
      [:title "UBC"]
      (when canonical-home
        [:script canonical-home-script])
-     (include-css "/css/style.css?v=20260729-2")]
+     (include-css "/css/style.css?v=20260729-3")]
     [:body
      [:div#header
       [:a {:href "/"}
